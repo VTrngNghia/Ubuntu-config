c0462430551e62e6ff3b94a08d1f5a9c2e2e682e
Trong Nghia <nghiavt1@coccoc.com>
2023-03-31 10:42:51 +0700

    zsh-custom

---
+++ zsh-custom/aliases.zsh
@@ -0,0 +1,18 @@
+echo "sourcing aliases from $(realpath $0)"
+
+alias refresh="source ~/.zshrc"
+alias tp="trash-put"
+alias v="nvim"
+alias py=python
+alias ts='ts-node'
+alias proxy-up='$HOME/scripts/proxy-up.sh'
+alias vsync="$HOME/scripts/sync-vertical-dev.sh"
+alias ssh-vertical-="ssh nghiavt1@verticals1v.dev.itim.vn"
+alias ssh-meta-="ssh nghiavt1@metasearcher1v.dev.itim.vn"
+alias ssh-searcher="ssh nghiavt1@searcher-data1v.dev.itim.vn"
+alias ssh-meta-prod="ssh nghiavt1@developer@metasearcher1v.itim.vn"
+alias ssync="$HOME/scripts/sync-searcher-data.sh"
+alias venv-init="python3 -m venv venv"
+alias ssh-home='ssh nghia@192.168.1.3'
+alias xcp='xclip -selection clipboard'
+alias cppfix='cpplint --do-fix'
