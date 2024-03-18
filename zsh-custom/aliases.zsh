7906b089a882020fd24680d3a843f566eaf502bb
Trong Nghia <nghiavt1@coccoc.com>
2024-03-18 12:58:23 +0700

    20240318

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -10,9 +10,16 @@ alias vsync="$HOME/scripts/sync-vertical-dev.sh"
 alias ssh-vertical-="ssh nghiavt1@verticals1v.dev.itim.vn"
 alias ssh-meta-="ssh nghiavt1@metasearcher1v.dev.itim.vn"
 alias ssh-searcher="ssh nghiavt1@searcher-data1v.dev.itim.vn"
-alias ssh-meta-prod="ssh nghiavt1@developer@metasearcher1v.itim.vn"
+alias ssh-meta-prod="ssh developer@metasearcher1v.itim.vn"
+alias ssh-vplanner-prod="ssh developer@vplanner1v.itim.vn"
+alias ssh-vplanner-prod2="ssh developer@vplanner2v.itim.vn"
+alias ssh-fsearcher-prod="ssh developer@fsearcher1v.itim.vn"
+alias ssh-fsearcher-prod2="ssh developer@fsearcher2v.itim.vn"
 alias ssync="$HOME/scripts/sync-searcher-data.sh"
 alias venv-init="python3 -m venv venv"
 alias ssh-home='ssh nghia@192.168.1.3'
 alias xcp='xclip -selection clipboard'
 alias cppfix='cpplint --do-fix'
+alias calibre-sync="/home/nghiavt1/scripts/calibre-sync.sh"
+alias gtree='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)" --all'
+
