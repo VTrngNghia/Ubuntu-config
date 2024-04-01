c853a72a35a3e32a9e61a50d62ee286797790325
nghiavt1 <nghiavt1@coccoc.com>
2024-04-01 11:18:17 +0700

    config nvim

--- zsh-custom/home/env.zsh
+++ zsh-custom/home/env.zsh
@@ -1,3 +1,10 @@
 echo zshenv loaded
 export sandbox=root@10.194.60.143
 
+PATH=/opt/jdk/jdk-15.0.1/bin:$PATH
+PATH=/snap/bin:$PATH
+PATH="$PATH:/home/nghia/.local/bin"
+PATH="/opt/apache-maven/bin:$PATH"
+export PATH
+
+
