9aa0fafdbf6b31a68e20d10ba69f4636382c5f7c
Trong Nghia <nghiavt1@coccoc.com>
2025-03-10 11:08:20 +0700

    zsh shortcuts

--- zsh-custom/env.zsh
+++ zsh-custom/env.zsh
@@ -1,9 +1,10 @@
 echo "Sourcing env from $(realpath $0)"
 
-export hweb="nghiavt1@hweb-indexer1v.dev.itim.vn"
 export gpu0=nghiavt1@10.0.8.27
 export gpu1=nghiavt1@10.0.9.42
+export hweb=nghiavt1@hweb-indexer1v.dev.itim.vn
 export sandbox="root@10.194.60.143"
+export sb=root@nghiavt1-sb.dev.virt
 export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 export QT_IM_MODULE=ibus
---
+++ zsh-custom/work/env.zsh
@@ -0,0 +1,3 @@
+echo "Sourcing env from $(realpath $0)"
+source "$HOME/.config/zsh-custom/env.zsh"
+
